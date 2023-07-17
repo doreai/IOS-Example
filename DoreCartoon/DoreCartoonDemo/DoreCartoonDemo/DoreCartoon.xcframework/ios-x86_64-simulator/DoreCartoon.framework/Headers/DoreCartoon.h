31436738f016cc238fdc0653a257768187298490
DoreAI <admin@dore.ai>
2023-07-17 10:02:05 +0530

    DoreCoreAI version update

---
+++ DoreCartoon/DoreCartoonDemo/DoreCartoonDemo/DoreCartoon.xcframework/ios-x86_64-simulator/DoreCartoon.framework/Headers/DoreCartoon.h
@@ -0,0 +1,18 @@
+//
+//  DoreCartoon.h
+//  DoreCartoon
+//
+//  Created by Sam on 04/12/21.
+//  Copyright © 2021 Dore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DoreCartoon.
+FOUNDATION_EXPORT double DoreCartoonVersionNumber;
+
+//! Project version string for DoreCartoon.
+FOUNDATION_EXPORT const unsigned char DoreCartoonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DoreCartoon/PublicHeader.h>
+
